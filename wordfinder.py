0c4ddcf82d41c368589fd7d7bff544eb3de46258
yceline-yu <cellystuff5@gmail.com>
2021-04-20 16:32:26 -0700

    special word finder

--- wordfinder.py
+++ wordfinder.py
@@ -1,13 +1,34 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
 
     def __init__(self, file_path):
+        """the file path provided"""
         self.file_path = file_path
         self.words = []
         self.read_file()
 
     def read_file(self):
+        """reads the file and appends to a list"""
         file = open(self.file_path)
         for line in file:
-            self.words.append(line)
+            self.words.append(line.replace("\n",""))
+        print(f"{len(self.words)} words read")
+
+    def random(self):
+        """return a random word from list"""
+        return choice(self.words)
+
+class SpecialWordFinder(WordFinder):
+    """Special Word Finder: handles blank lines and lines that start with #"""
+
+    def read_file(self):
+        """handles blanks and lines that start #"""
+        file = open(self.file_path)
+        for line in file:
+            if line == "\n" or line[0] == "#": 
+                continue
+            else:
+                self.words.append(line.replace("\n",""))
         print(f"{len(self.words)} words read")
