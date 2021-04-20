0fc28797e10ce3aa6131ba30db95afbc31471f2b
yceline-yu <cellystuff5@gmail.com>
2021-04-20 15:30:16 -0700

    serial methods added

--- serial.py
+++ serial.py
@@ -17,3 +17,17 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self,start):
+        """the starting number"""
+        self.start = start
+        self.counter = 0
+
+    def generate(self):
+        """returns the next sequential number"""
+        self.counter += 1
+        return (self.start - 1) + self.counter
+
+    def reset(self):
+        """reset the number back to the original start"""
+        self.counter = 0
+
