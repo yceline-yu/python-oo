635926e8e03cc0d5bba70104b92d73ff10069b79
Matt Riese <rise.matt@gmail.com>
2021-04-20 16:00:23 -0700

    init and read_file are working in wordfinder.py

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,13 @@
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.words = []
+        self.read_file()
+
+    def read_file(self):
+        file = open(self.file_path)
+        for line in file:
+            self.words.append(line)
+        print(f"{len(self.words)} words read")
